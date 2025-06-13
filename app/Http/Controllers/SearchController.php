d950de6f7d495bff36d89dc86690f22a57c909e7
Obada Sabbagh <badasabbagh@gmail.com>
2025-06-13 13:08:18 +0300

    fix

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -12,34 +12,25 @@ class SearchController extends Controller
 public function search(Request $request)
 {
     $search = $request->input('query');
-    if (is_array($search)) {
-        return response()->json([
-            'message' => 'Search word must be a string, not an array',
-            'users' => [],
-            'posts' => []
-        ], 400);
-    }
     if (empty($search)) {
         return response()->json([
             'message' => 'Search word is required',
             'users' => [],
-            'posts' => []
         ], 400);
     }
-    $posts = Post::with('User')
-                ->withCount('reactions')
-                ->withCount('comment')->where('content', 'LIKE', '%'.$search.'%')
-                ->limit(20)
-                ->get();
     $users = User::where('firstname', 'LIKE', '%'.$search.'%')
                 ->where('role',['user','police'])
                 ->orWhere('lastname', 'LIKE', '%'.$search.'%')
                 ->limit(10)
                 ->get();
+                if($users->isEmpty()){
+                    return response()->json([
+                        'message' => 'لا يوجد نتائج',
+                    ]);
+                }
     return response()->json([
         'message' => 'Search results',
         'users' => $users,
-        'posts' => $posts
     ]);
 }
 }
