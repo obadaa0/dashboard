42b0d17bbe99ac2601b2874adf89707bc9a0554a
Obada Sabbagh <badasabbagh@gmail.com>
2025-06-16 07:54:56 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\reportPostController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReportController;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
